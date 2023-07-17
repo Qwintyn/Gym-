e3fb2025911a61a06f1924a4cbd6ec482dfce442
Qwintyn <q.rice17@hotmail.com>
2023-07-16 22:44:56 -0400

    First commit

--- Db.py
+++ Db.py
@@ -2,5 +2,6 @@ import mysql.connector
 
 
 def connect():
+    #place in a try/catch
     mydb = mysql.connector.connect(host='localhost', port=3306, user='root', password='football', database='gym')
     return mydb
--- app.py
+++ app.py
@@ -1,30 +1,48 @@
-from flask import Flask
+from flask import Flask, request
 import Db, mysql.connector
+
 app = Flask(__name__)
 myDb = Db.connect()
 
 @app.route('/Users')
 def get_all_users():
     my_cursor = myDb.cursor()
-
     my_cursor.execute('SELECT * FROM USER')
-
     users = my_cursor.fetchall()
-
     return users
 
-@app.route('/Users/<id>')
+
+@app.route('/User/<id>', methods=['GET'])
 def get_user(id):
     my_cursor = myDb.cursor()
     # Search why you bind params in sql
-    my_cursor.execute('SELECT * FROM USER WHERE user_id = %s', list(id))
-
+    # to pass values into a query without placing the value itself in the sql statement
+    sql1 ='SELECT * FROM USER WHERE user_id = %s'
+    my_cursor.execute(sql1, list(id))
     user = my_cursor.fetchall()
-    # investigate  into fetchone why it returns something weird
     return user
+    # investigate  into fetchone why it returns something weird
+    # fetchall = will return all rows
+    # fetchone = will return one row at a time
 
-@app.route('/vipMembers')
-def get_vips():
+
+@app.route('/vipMembers/<id>')
+def get_vips(id):
+    my_cursor = myDb.cursor()
+    sql2 = 'SELECT * FROM VIP_member WHERE user_id = %s'
+    my_cursor.execute(sql2, list(id))
+    vip = my_cursor.fetchone()
+    return {'Vip member': vip}
+# second make sure the primary key (id) is foreign key of userid
+
+@app.route('/User/<id>', methods = ['DELETE'] )
+def delete_user(id):
+    my_cursor = myDb.cursor()
+    sql3 = 'DELETE FROM USER where user_id = %s '
+    my_cursor.execute(sql3, list(id))
+    myDb.commit()
+    return {'Message': 'User deleted'}
+
 
 
 
